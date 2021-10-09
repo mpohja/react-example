63218d186d998c226c66056582c39e1dc4db0ed2
Mikko Pohja <mikko.pohja@bytecraft.fi>
2021-10-09 13:30:07 +0300

    Lifting state to parent

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React from 'react';
 
-class Square extends React.Component {
+const Square = ({value, onClick}) => (
+  <button className="square" onClick={onClick}>
+    {value}
+  </button>
+);
+
+class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: null,
+      squares: Array(9).fill(null),
     };
   }
 
-  render() {
-    return (
-      <button className="square" onClick={() => this.setState({value: 'X'})}>
-        {this.state.value}
-      </button>
-    );
+  handleClick(i) {
+    const squares = this.state.squares.slice()
+    squares[i] = 'X'
+    this.setState({squares: squares})
   }
-}
 
-class Board extends React.Component {
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
